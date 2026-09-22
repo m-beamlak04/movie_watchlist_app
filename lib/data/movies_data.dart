b39a14aed308ddfcdc03947bd175e1595cb86997
Beamlak <beamlak840tedla@gmail.com>
2026-09-22 17:42:34 -0400

    added movie model and sample movie data

---
+++ lib/data/movies_data.dart
@@ -0,0 +1,44 @@
+import '../models/movie.dart';
+
+final List<Movie> sampleMovies = [
+  Movie(
+      title: 'Inception',
+      posterPath: 'assets/images/inception.jpg',
+      cast: ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Elliot Page'],
+      synopsis: 'A thief who steals secrets through dream-sharing technology is '
+          'given a chance to have his past crimes forgiven if he can plant an '
+          'idea into a target\'s mind.',
+  ),
+
+  Movie(
+      title: 'Interstellar',
+      posterPath: 'assets/images/interstellar.jpg',
+      cast: ['Matthew McConaughey', 'Anne Hathaway', 'Jessica Chastain'],
+      synopsis: 'A group of astronauts travels through space searching for '
+          'a new home for humanity.',
+  ),
+
+  Movie(
+      title: 'Sinners',
+      posterPath: 'assets/images/sinners.jpg',
+      cast: ['Michael B. Jordan', 'Delroy Lindo', 'Hailee Steinfeld'],
+      synopsis: 'Twin brothers return to their hometown hoping to start over '
+          'but encounter a supernatural threat.',
+  ),
+
+  Movie(
+      title: 'Weapons',
+      posterPath: 'assets/images/weapons.jpg',
+      cast: ['Josh Brolin', 'Amy Madigan', 'Julia Garner'],
+      synopsis: 'A community searches for answers after a group of children '
+          'mysteriously disappears.',
+  ),
+
+  Movie(
+      title: 'It',
+      posterPath: 'assets/images/it.jpg',
+      cast: ['Bill Skarsgard', 'Jaeden Martell', 'Finn Wolfhard'],
+      synopsis: 'A group of kids faces a terrifying creature that takes the '
+          'form of their greatest fears.',
+  ),
+];
